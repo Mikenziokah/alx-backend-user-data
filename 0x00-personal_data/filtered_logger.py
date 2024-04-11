b4d0ef6895286d998780a50b5c5fc19198fd98c5
Mikenziokah <mikenziokah@gmail.com>
2024-04-11 19:42:26 +0000

    reduction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """ function called filter_datum that returns the log message
-    obfuscated
 """
 import re
 from typing import List
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ Replacing """
-    return re.sub(
-        rf"({'|'.join(map(re.escape, fields))})=[^{separator}]+{re.escape(separator)}",
-        rf"\1={redaction}{separator}",
-        message
-    )
+    for f in fields:
+        message = re.sub(rf"{f}=(.*?)\{separator}",
+                         f'{f}={redaction}{separator}', message)
+    return message
