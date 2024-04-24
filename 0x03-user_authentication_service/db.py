8fb96ce01c6fb8bac03fcf6f19d597269afff310
Mikenziokah <mikenziokah@gmail.com>
2024-04-24 18:47:14 +0000

    db

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,30 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self):
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
