e0c7038e558a39d958661c5e4d0cb57d0669f1ec
Mikenziokah <mikenziokah@gmail.com>
2024-04-25 18:52:18 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,6 +17,12 @@ def _hash_password(password: str) -> str:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """ generating a new uuid
+    """
+    return str(uuid())
+
+
 class Auth:
     """
     Auth class to interact with the authentication database.
@@ -24,11 +30,6 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
-
-    def _generate_uuid() -> str:
-        """ generating a new uuid
-        """
-        return str(uuid4())
 
     def register_user(self, email: str, password: str) -> User:
         """
