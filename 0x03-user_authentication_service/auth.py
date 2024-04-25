38fad065006dca274f50482625c0e98eccca7047
Mikenziokah <mikenziokah@gmail.com>
2024-04-25 18:37:49 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -25,22 +25,20 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
+    def _generate_uuid(self) -> str:
+        """ generating a new uuid
+        """
+        return str(uuid4())
 
-def _generate_uuid() -> str:
-    """ generating a new uuid
-    """
-    return str(uuid4())
-
-
-def register_user(self, email: str, password: str) -> User:
-    """
-    register_user.
-    """
-    try:
-        self._db.find_user_by(email=email)
-        raise ValueError(f"User {email} already exists")
-    except NoResultFound:
-        return self._db.add_user(email, _hash_password(password))
+    def register_user(self, email: str, password: str) -> User:
+        """
+        register_user.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
 
     def valid_login(self, email: str, password: str) -> bool:
         """
